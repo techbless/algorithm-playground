52af8e689bb9053e85b2ede0cf02abe6935ed5af
Yunbin Chang <hw@yunbin.kr>
2020-02-12 19:26:34 +0900

    Update Queue.c

--- Queue.c
+++ Queue.c
@@ -2,43 +2,43 @@
 #define MAX 10
 
 typedef struct {
-	int container[MAX];
-	int front;
-	int rear;
+    int container[MAX];
+    int front;
+    int rear;
 } queue;
 
 queue getNewQueue() {
-	queue newQ;
-	newQ.front = -1;
-	newQ.rear = -1;
+    queue newQ;
+    newQ.front = -1;
+    newQ.rear = -1;
 	
-	return newQ;
+    return newQ;
 }
 
 void enqueue(queue* Q, int value) {
-	Q->container[++Q->rear] = value;
+    Q->container[++Q->rear] = value;
 }
 
 int dequeue(queue* Q) {
-	if (Q->front == Q->rear) {
-		printf("This Queue is empty, return value should be ignored.");
-		return -1;
-	}
+    if (Q->front == Q->rear) {
+        printf("This Queue is empty, return value should be ignored.");
+        return -1;
+    }
 
-	return Q->container[++Q->front];
+    return Q->container[++Q->front];
 }
 
 int main(void) {
-	queue q1 = getNewQueue();
+    queue q1 = getNewQueue();
 
-	for (int i = 0; i < 10; i++) {
-		enqueue(&q1, i);
-		printf("Enqueue : %d\n", i);
-	}
+    for (int i = 0; i < 10; i++) {
+        enqueue(&q1, i);
+        printf("Enqueue : %d\n", i);
+    }
 
-	for (int i = 0; i < 10; i++) {
-		printf("Dequeue : %d\n", dequeue(&q1));
-	}
+    for (int i = 0; i < 10; i++) {
+        printf("Dequeue : %d\n", dequeue(&q1));
+    }
 
-	return 0;
+    return 0;
 }
