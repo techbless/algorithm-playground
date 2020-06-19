d4473012ca30ea24dacf872e98446eb9eb006f6b
Yunbin Chang <yunbin@hansung.ac.kr>
2020-06-20 01:10:04 +0900

    Update Queue.c

--- DataStructure/Queue.c
+++ DataStructure/Queue.c
@@ -1,43 +1,100 @@
 #include <stdio.h>
-#define MAX 10
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX_SIZE 1000
+
+typedef int element;
 
 typedef struct {
-    int container[MAX];
+    element* data;
+    int maxSize;
     int front;
     int rear;
-} queue;
+} Queue;
 
-queue getNewQueue() {
-    queue newQ;
-    newQ.front = -1;
-    newQ.rear = -1;
-	
-    return newQ;
+Queue create(int size) {
+    Queue queue;
+    queue.data = (element*)malloc(sizeof(element) * size);
+    queue.maxSize = size;
+    queue.front = 0;
+    queue.rear = 0;
+
+    return queue;
 }
 
-void enqueue(queue* Q, int value) {
-    Q->container[++Q->rear] = value;
-}
-
-int dequeue(queue* Q) {
-    if (Q->front == Q->rear) {
-        printf("This Queue is empty, return value should be ignored.");
-        return -1;
+bool isEmpty(Queue* queue) {
+    if (queue->front == queue->rear) {
+        return true;
     }
+    else {
+        return false;
+    }
+}
 
-    return Q->container[++Q->front];
+bool isFull(Queue* queue) {
+    if (queue->rear >= queue->maxSize) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool enqueue(Queue* queue, int data) {
+    if (isFull(queue)) {
+        return false;
+    }
+    else {
+        queue->data[++(queue->rear)] = data;
+        return true;
+    }
+}
+
+element dequeue(Queue* queue) {
+    if (isEmpty(queue)) {
+        printf("Queue가 비어있습니다.\n");
+        return -999;
+    }
+    else {
+        return queue->data[++(queue->front)];
+    }
+}
+
+void print(Queue* queue) {
+    printf("[ ");
+    for (int i = queue->front+1; i <= queue->rear; i++) {
+        printf("%d", queue->data[i]);
+        if (i < queue->rear) {
+            printf(", ");
+        }
+    }
+    printf(" ]\n");
 }
 
 int main(void) {
-    queue q1 = getNewQueue();
+    Queue queue = create(10);
 
-    for (int i = 0; i < 10; i++) {
-        enqueue(&q1, i);
-        printf("Enqueue : %d\n", i);
+    printf("NEW: ");
+    print(&queue);
+    printf("\n");
+    
+    for (int i = 0; i < 5; i++) {
+        if (enqueue(&queue, i)) {
+            printf("%d -> ", i);
+            print(&queue);
+        }
+        else {
+            printf("큐가 가득 찼습니다.\n");
+        }
     }
 
-    for (int i = 0; i < 10; i++) {
-        printf("Dequeue : %d\n", dequeue(&q1));
+    printf("\n");
+    
+    int value;
+    for (int i = 0; i < 5; i++) {
+        value = dequeue(&queue);
+        printf("%d <- ", value);
+        print(&queue);
     }
 
     return 0;
